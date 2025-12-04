7390444fbcf32bea3e32968a03f3d56627427791
elbryotann808 <137430208+elbryotann808@users.noreply.github.com>
2025-12-03 20:21:13 -0500

    Se arreglo configuracion de astro para el despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://elbryotann808.github.io',
+  base: '/portfolio', 
   integrations: [icon()],
   devToolbar: {
     enabled: false
